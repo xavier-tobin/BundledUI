de30e3a55a1452eb1c48e6e652d590c897d4f31e
Xavier Tobin <xaviertobin@reveried.com>
2025-09-04 08:28:14 +1000

    Update SectionSlider.kt

--- src/main/java/com/xaviertobin/bundledui/section/widgets/SectionSlider.kt
+++ src/main/java/com/xaviertobin/bundledui/section/widgets/SectionSlider.kt
@@ -2,21 +2,18 @@ package com.xaviertobin.bundledui.section.widgets
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.xaviertobin.bundledui.base.ComposableFunction
 import com.xaviertobin.bundledui.base.UnitFunction
-import com.xaviertobin.bundledui.section.base.Section
 
-
-// TODO move to section title description
 @Composable
 fun SectionSlider(
     text: String,
@@ -32,53 +29,38 @@ fun SectionSlider(
     last: Boolean = false,
 ) {
 
-    Section(
+    SectionTitleDescription(
         first = first,
         last = last,
+        title = text,
+        description = description,
         modifier = modifier,
-    ) {
-        Text(
-            text = text,
-            style = MaterialTheme.typography.bodyLarge,
-            color = MaterialTheme.colorScheme.onSurfaceVariant,
-            textAlign = TextAlign.Start,
-            modifier = Modifier
-                .padding(bottom = 4.dp, start = 4.dp)
+        contentBottom = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Column(
+                    modifier = Modifier
+                        .padding(
+                            top = 6.dp
+                        )
+                        .weight(1f)
+                ) {
 
-        )
-        description?.let {
-            Text(
-                text = description,
-                style = MaterialTheme.typography.bodySmall,
-                color = MaterialTheme.colorScheme.onSurfaceVariant,
-                textAlign = TextAlign.Start,
-                modifier = Modifier.padding(start = 4.dp)
-            )
-        }
-
-        Row {
-            Column(
-                modifier = Modifier
-                    .padding(
-                        end = 10.dp,
-                        top = 4.dp
-                    ).weight(1f)
-            ) {
-
-                Slider(
-                    value = value,
-                    onValueChange = onValueChange,
-                    onValueChangeFinished = onValueChangeFinished,
-                    colors = SliderDefaults.colors(
-                        thumbColor = MaterialTheme.colorScheme.tertiary,
-                        activeTrackColor = MaterialTheme.colorScheme.tertiary,
-                        inactiveTrackColor = MaterialTheme.colorScheme.tertiary.copy(0.2f),
-                    ),
-                    valueRange = range,
-                    steps = steps,
-                )
+                    Slider(
+                        value = value,
+                        onValueChange = onValueChange,
+                        onValueChangeFinished = onValueChangeFinished,
+                        colors = SliderDefaults.colors(
+                            thumbColor = MaterialTheme.colorScheme.tertiary,
+                            activeTrackColor = MaterialTheme.colorScheme.tertiary,
+                            inactiveTrackColor = MaterialTheme.colorScheme.tertiary.copy(0.2f),
+                        ),
+                        valueRange = range,
+                        steps = steps,
+                        modifier = Modifier.heightIn(40.dp)
+                    )
+                }
+                valueDisplay?.invoke()
             }
-            valueDisplay?.invoke()
         }
-    }
+    )
 }
